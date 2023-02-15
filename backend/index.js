fb8fe9b4ff95d49ca70b506ada4c1c767db3933e
Darshan Tailor <darshanhtailor@gmail.com>
2023-02-15 12:07:49 +0530

    init

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const connectToAtlas = require("./db")
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 5000
+connectToAtlas();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/notes", require("./routes/notes"))
+app.use("/api/admin", require("./routes/admin"))
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`)
+})
+
+
---
+++ backend/routes/notes.js
@@ -0,0 +1,78 @@
+const express = require("express")
+const Notes = require("../models/Notes")
+const fetchuser = require("../middlewares/fetchUser");
+const { body, validationResult } = require('express-validator');
+const multer = require('multer');
+const router = express.Router()
+
+// getting all the notes
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+})
+
+// add a new notes
+const upload = multer({ dest: "public/files" });
+router.post("/addnote", fetchuser, [
+    upload.single('myimage')
+], async (req, res) => {
+    try {
+
+        // checking validation and errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { title, description, tag } = req.body;
+        console.log(req.file);
+        newnote = await Notes.create({
+            user: req.user.id,
+            title,
+            description,
+            tag
+        })
+        res.json(newnote)
+    } catch (error) {
+        res.status(500).send("Error has occured" + error);
+    }
+})
+
+// update an existing note
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+    try {
+        const {title, description, tag, } = req.body;
+        // create a new note object
+        newnote = {};
+        if(title) newnote.title = title
+        if(description) newnote.description = description
+        if(tag) newnote.tag = tag
+    
+        // checking is this same user who entered notes
+        const n = await Notes.findById(req.params.id)
+        if(!n) return res.status(404).send("Not found")
+        if(n.user.toString() !== req.user.id && req.user.role !== "admin") return res.send(401).send("Unauthorized")
+    
+        // find the note to be updated and update it
+        const note = await Notes.findByIdAndUpdate(req.params.id, {$set: newnote}, {new: true})
+        res.json(note);
+    } catch (error) {
+        res.status(500).send("Error has occured" + error);
+    }
+})
+
+// deleting an existing note
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+    try {
+        // checking is this same user who entered notes
+        const n = await Notes.findById(req.params.id)
+        if(!n) return res.status(404).send("Not found..!!")
+        if(n.user.toString() !== req.user.id && req.user.role !== "admin") return res.send(401).send("Unauthorized")
+        // find the note to be updated and update it
+        const note = await Notes.findByIdAndDelete(req.params.id);
+        res.send("Note has been deleted");
+    } catch (error) {
+        res.status(500).send("Error has occured" + error);
+    }
+})
+
+module.exports = router
